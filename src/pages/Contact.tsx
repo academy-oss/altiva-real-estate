8b148fc9f1e0a4190e641077da0e36cbf2e0b5eb
academy-oss <academy@altivaproperties.com>
2026-09-11 07:51:51 +0400

    Add info email to contact page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -118,6 +118,12 @@ export default function Contact() {
                   <bdi dir="ltr">sales@altivaproperties.com</bdi>
                 </a>
               </li>
+              <li className="flex flex-col items-center gap-2 text-center">
+                <Mail size={18} className="shrink-0 text-copper" />
+                <a href="mailto:info@altivaproperties.com" className="text-center hover:text-copper">
+                  <bdi dir="ltr">info@altivaproperties.com</bdi>
+                </a>
+              </li>
             </ul>
           </div>
         </div>
